eab6ad5e87518ff0b14b74c45b74f485b1985daa
Jhaym3s <jhaymescode@gmail.com>
2026-06-02 18:14:22 +0100

    8080 server

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
 
 	"github.com/jhaym3s/udua/cmd"
@@ -23,7 +24,12 @@ func main(){
 
 	h := api.Mount()
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	slog.SetDefault(logger)// this is the standard library logger, so you can use slog.Info, slog.Error, etc. anywhere in your code
+
 	if err := api.Run(h); err != nil {
+		slog.Error("Server failed to start: ", err)
 		log.Println("Server Failed to start:", err)
 		log.Fatal()
 		os.Exit(1)
